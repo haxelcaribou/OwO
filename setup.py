9f446366d8c4e7ba089a93308134a7e9f3f39162
piethrower <piethrowerchamp@gmail.com>
2021-03-30 23:14:22 -0400

    Fixed long description bug

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    README_md = f.read()
+
 setup(
     name="text-to-owo",
-    version="1.0.0",
+    version="1.0.1",
     author="Pie Thrower",
     author_email="piethrowerchamp@gmail.com",
     description="Translates normal text into OwO speak. Port from Zuzak's javascript version",
-    long_description="file: README.md",
+    long_description=README_md,
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/piethrower/OwO",
