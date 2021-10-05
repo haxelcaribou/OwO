596975888fc7f17443ed843b9629bba1bd510a96
piethrower <piethrowerchamp@gmail.com>
2021-10-04 21:32:19 -0400

    updated tests to use unittest module

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
+#!/usr/bin/python3
+
+import unittest
+
 import owo
 
+# assert(owo.add_prefix(str,prefixes=("test",)) == "test"+str)
+# assert(owo.add_suffix(str,suffixes=("test",)) == str+"test")
+# assert(owo.substitute(str,substitutions={"123":"abc"}) == "abc456789")
+#
+# owo.add_suffix(str,suffixes="test")
+# owo.add_preffix(str,suffixes=("test",))
+
+
+str = "123456789"
+affix = ("abcdefg",)
 subs = {
     "l": "w",
     "lllll": "wwwww",
@@ -11,12 +25,82 @@ subs = {
     "MPs weject Thewesa May's wevised EU withdwawaw deaw, thwowing UK's Bwexit pwans into confusion"
 }
 
-for x in subs:
-    print(owo.substitute(x) == subs[x])
 
-str = "123456789"
-print(len(owo.add_affixes(str)) >= len(str))
+class TestOwO(unittest.TestCase):
 
-print(owo.add_prefix(str,prefixes=("test",)) == "test"+str)
-print(owo.add_suffix(str,suffixes=("test",)) == str+"test")
-print(owo.substitute(str,substitutions={"123":"abc"}) == "abc456789")
+    def test_constants(self):
+        self.assertIsInstance(owo.SUBSTITUTIONS, dict)
+        self.assertIsInstance(owo.PREFIXES, tuple)
+        self.assertIsInstance(owo.SUFFIXES, tuple)
+
+    def test_substitution(self):
+        for x in subs:
+            self.assertEqual(owo.substitute(x), subs[x])
+        with self.assertRaises(TypeError):
+            owo.substitute(30)
+        with self.assertRaises(TypeError):
+            owo.substitute(str, substitutions=30)
+        with self.assertRaises(TypeError):
+            owo.substitute(str, substitutions="string")
+        self.assertEqual(owo.substitute(
+            str, substitutions={"123": "xyz"}), "xyz456789")
+
+    def test_affixes(self):
+        self.assertTrue(len(owo.add_affixes(str)) >= len(str))
+        self.assertEqual(owo.add_affixes(str, prefixes=affix,
+                         suffixes=affix), "abcdefg123456789abcdefg")
+        with self.assertRaises(TypeError):
+            owo.add_affixes(30)
+        with self.assertRaises(TypeError):
+            owo.add_affixes(str, prefixes=30)
+        with self.assertRaises(TypeError):
+            owo.add_affixes(str, prefixes="string")
+        with self.assertRaises(TypeError):
+            owo.add_affixes(str, suffixes=30)
+        with self.assertRaises(TypeError):
+            owo.add_affixes(str, suffixes="string")
+
+    def test_prefixes(self):
+        self.assertTrue(len(owo.add_prefix(str)) >= len(str))
+        self.assertEqual(owo.add_prefix(
+            str, prefixes=affix), "abcdefg123456789")
+        with self.assertRaises(TypeError):
+            owo.add_prefix(30)
+        with self.assertRaises(TypeError):
+            owo.add_prefix(str, prefixes=30)
+        with self.assertRaises(TypeError):
+            owo.add_prefix(str, prefixes="string")
+
+    def test_suffixes(self):
+        self.assertTrue(len(owo.add_suffix(str)) >= len(str))
+        self.assertEqual(owo.add_suffix(
+            str, suffixes=affix), "123456789abcdefg")
+        with self.assertRaises(TypeError):
+            owo.add_suffix(30)
+        with self.assertRaises(TypeError):
+            owo.add_suffix(str, suffixes=30)
+        with self.assertRaises(TypeError):
+            owo.add_suffix(str, suffixes="string")
+
+    def test_owo(self):
+        with self.assertRaises(TypeError):
+            owo.owo(30)
+        with self.assertRaises(TypeError):
+            owo.owo(str, substitutions=30)
+        with self.assertRaises(TypeError):
+            owo.owo(str, substitutions="string")
+        with self.assertRaises(TypeError):
+            owo.owo(str, prefixes=30)
+        with self.assertRaises(TypeError):
+            owo.owo(str, prefixes="string")
+        with self.assertRaises(TypeError):
+            owo.owo(str, suffixes=30)
+        with self.assertRaises(TypeError):
+            owo.owo(str, suffixes="string")
+        self.assertEqual(owo.owo(str, prefixes=affix, suffixes=affix, substitutions={
+                         "123": "xyz"}), "abcdefgxyz456789abcdefg")
+
+
+if __name__ == '__main__':
+    # Main module
+    unittest.main()
